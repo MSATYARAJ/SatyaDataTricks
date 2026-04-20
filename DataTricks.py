772c0108a7cd0db69d844c00fef092b0f6f44f78
MSATYARAJ <154305340+MSATYARAJ@users.noreply.github.com>
2026-04-20 22:05:43 +0530

    Adjust logo image width to 200 pixels
    Increased the width of the logo image on the main page.

--- DataTricks.py
+++ DataTricks.py
@@ -14,7 +14,7 @@ st.logo("image_cb68b62a.png", link="https://streamlit.app")
 col1, col2 = st.columns([1, 4])
 
 with col1:
-    st.image("image_cb68b62a.png",width=100) # Adjust width as needed
+    st.image("image_cb68b62a.png",width=200) # Adjust width as needed
 
 with col2:
     st.title("LinkLab")
