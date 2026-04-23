de736fc82773e32acb54c127841c9fb0a3353cb5
MSATYARAJ <154305340+MSATYARAJ@users.noreply.github.com>
2026-04-23 17:31:27 +0530

    Fix import statement for python_calamine module

--- DataTricksNew.py
+++ DataTricksNew.py
@@ -6,7 +6,7 @@ import sqlite3
 import os
 import textwrap
 import zipfile
-from python-calamine import CalamineWorkbook
+from python_calamine import CalamineWorkbook
 
 # --- 1. GLOBAL CONFIGURATION ---
 st.set_page_config(
